3fbc5ea8cf4db310ebf20872e6437c7074a9ee42
Fady Alamir <fadyalamer72@gmail.com>
2026-01-03 23:01:38 +0200

    fix: serverless configuration for vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+const express = require('express');
 
 const server = express();
 
