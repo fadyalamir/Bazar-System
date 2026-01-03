67af89fb54c23875a8063fae1d7a18b03127e696
Fady Alamir <fadyalamer72@gmail.com>
2026-01-03 22:59:28 +0200

    fix: serverless configuration for vercel deployment

--- app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class AppController {
-  @Get()
-  getHello(): string {
-    return '🚀 Bazar API is running successfully!';
-  }
-}
